dda265cce08bca5b541490eeb7f2e7e89bb85907
josephine anyango <anyangojozey@gmail.com>
2025-11-19 09:58:47 +0300

    done

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,16 @@
-def write_file(file_name, file_content):
-    pass
+def write_file(file_name, content):
+    file_path = f"{file_name}.txt"
+    with open(file_path, "w") as f:
+        f.write(content)
 
-def append_file(file_name, append_content):
-    pass
+
+def append_file(file_name, content):
+    file_path = f"{file_name}.txt"
+    with open(file_path, "a") as f:
+        f.write(content)
+
 
 def read_file(file_name):
-    pass
+    file_path = f"{file_name}.txt"
+    with open(file_path, "r") as f:
+        return f.read()
